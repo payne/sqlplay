01f6264cfb9ddbf3d9cd9d28857065968c66cb6f
Matt Payne <Payne@MattPayne.org>
2016-07-23 10:53:53 -0500

    start.spring.io

---
+++ src/main/java/org/mattpayne/springboot/sql/SqlplayApplication.java
@@ -0,0 +1,12 @@
+package org.mattpayne.springboot.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlplayApplication.class, args);
+	}
+}
