d4ba2a0d24270e90e7d0643e1850df1eecfc9a44
Matt Payne <Payne@MattPayne.org>
2016-08-12 20:56:24 -0500

    It is an application and a servlet!

--- src/main/java/org/mattpayne/springboot/sql/servlets/FooServlet.java
+++ src/main/java/org/mattpayne/springboot/sql/servlets/FooServlet.java
@@ -1,8 +1,8 @@
 package org.mattpayne.springboot.sql.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,11 +10,31 @@ import javax.servlet.http.HttpServletResponse;
 public class FooServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	public static void main(String[] args) {
+		System.out.println("Hello!  I am a servlet and an applciation.");
+		PrintWriter out = new PrintWriter(System.out);
+		square(out);
+		out.flush();
+	}
+
+	private static void square(PrintWriter out) {
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
+				// TODO: Use font awesome!
+				out.print("*");
+			}
+			out.println();
+		}
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
-		ServletOutputStream out = resp.getOutputStream();
+		PrintWriter out = resp.getWriter();
 		out.println("<h1>It is now " + new java.util.Date() + "</h1>");
-
+		out.println("<pre>");
+		square(out);
+		out.println("</pre>");
+		out.flush();
 	}
 
 }
